2bbb3218139954d31739f4ffb6a532b764a6f5a5
ashik-billah <ashik.210101@s.pust.ac.bd>
2025-12-03 17:08:21 +0600

    added auth provider component

--- src/contexts/AuthContext/AuthProvider.jsx
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
 import { auth } from '../../firebase/firebase.init';
-
+//add
 const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {

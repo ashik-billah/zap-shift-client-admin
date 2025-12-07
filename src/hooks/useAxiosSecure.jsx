85213426dc640faccdc6cbd03375c225dfa19066
ashik-billah <ashik.210101@s.pust.ac.bd>
2025-12-07 19:31:33 +0600

    added useaxious secure

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { useEffect } from 'react';
 import useAuth from './useAuth';
 import { useNavigate } from 'react-router';
-
+//added
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:3000'
 })

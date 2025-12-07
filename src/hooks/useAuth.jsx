53caa90e52f71f599c8f99f65a38364097793186
ashik-billah <ashik.210101@s.pust.ac.bd>
2025-12-07 19:29:07 +0600

    added use auth

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,6 +1,7 @@
 import React, { use } from 'react';
 import { AuthContext } from '../contexts/AuthContext/AuthContext';
-
+//added
+//addd
 const useAuth = () => {
     const authInfo = use(AuthContext);
     return authInfo;

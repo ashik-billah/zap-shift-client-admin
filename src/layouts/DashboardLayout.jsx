53531a9824cc5a8e46cd14005d5f05e1ec6a5057
ashik-billah <ashik.210101@s.pust.ac.bd>
2025-12-12 12:20:38 +0600

    added sidebar content

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -28,7 +28,7 @@ const DashboardLayout = () => {
             <div className="drawer-side is-drawer-close:overflow-visible">
                 <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
                 <div className="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-64">
-                    {/* Sidebar content here */}
+                    {/* Sidebar content here added */}
                     <ul className="menu w-full grow">
                         {/* List item */}
                         <li>

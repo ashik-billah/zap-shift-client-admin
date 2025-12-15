44095ae997ddccd6223ffb9fca7ec571b8e88573
ashik-billah <ashik.210101@s.pust.ac.bd>
2025-12-15 16:39:50 +0600

    sequesnce of layout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router';
 import Footer from '../pages/Shared/Footer/Footer';
 import NavBar from '../pages/Shared/NavBar/NavBar';
 //added
+//sequesnce of layout
 const RootLayout = () => {
     return (
         <div className='max-w-7xl mx-auto'>

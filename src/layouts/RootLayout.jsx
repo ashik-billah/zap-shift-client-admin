20d9e026bd4d06f7bd20c5981df1a5095dd04bb6
ashik-billah <ashik.210101@s.pust.ac.bd>
2025-12-15 16:38:57 +0600

    added rootlyaout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import Footer from '../pages/Shared/Footer/Footer';
 import NavBar from '../pages/Shared/NavBar/NavBar';
-
+//added
 const RootLayout = () => {
     return (
         <div className='max-w-7xl mx-auto'>

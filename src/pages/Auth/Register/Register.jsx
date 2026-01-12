b6b56a124ac101b08dc7508b609c84d046a592cb
ashik-billah <ashik.210101@s.pust.ac.bd>
2026-01-12 20:28:28 +0600

    afj

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -81,7 +81,7 @@ const Register = () => {
                     <input type="text" {...register('name', { required: true })} className="input" placeholder="Your Name" />
                     {errors.name?.type === 'required' && <p className='text-red-500'>Name is required.</p>}
 
-                    {/* photo image field */}
+                    {/* photo image field added*/}
                     <label className="label">Photo</label>
 
                     <input type="file" {...register('photo', { required: true })} className="file-input" placeholder="Your Photo" />

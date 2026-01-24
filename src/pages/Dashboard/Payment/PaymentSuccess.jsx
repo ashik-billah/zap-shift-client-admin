a95bda767b8ef7fd5944bc4be45cb0fa82daa916
ashik-billah <ashik.210101@s.pust.ac.bd>
2026-01-24 10:33:29 +0600

    added payment success component

--- src/pages/Dashboard/Payment/PaymentSuccess.jsx
+++ src/pages/Dashboard/Payment/PaymentSuccess.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
-
+//added
 const PaymentSuccess = () => {
     const [searchParams] = useSearchParams();
     const [paymentInfo, setPaymentInfo] = useState({});

b053467ee3dec21192012e5cb9ecade43d97078c
ashik-billah <ashik.210101@s.pust.ac.bd>
2026-01-31 12:59:52 +0600

    added brand component

--- src/pages/Home/Brands/Brands.jsx
+++ src/pages/Home/Brands/Brands.jsx
@@ -9,6 +9,7 @@ import randstad from '../../../assets/brands/randstad.png';
 import star from '../../../assets/brands/star.png';
 import start_people from '../../../assets/brands/start_people.png';
 import { Autoplay } from 'swiper/modules';
+//added
 
 const brandLogos = [amazon, amazon_vector, casio, moonstar, randstad, star, start_people];
 

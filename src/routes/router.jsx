2579ef122eec8c8374f301dd16c8ffd4ef7b937d
ashik-billah <ashik.210101@s.pust.ac.bd>
2026-03-31 20:23:11 +0600

    add coverage

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -20,6 +20,8 @@ import AdminRoute from "./AdminRoute";
 import AssignRiders from "../pages/Dashboard/AssignRiders/AssignRiders";
 //add
 //add
+//add
+//add
 export const router = createBrowserRouter([
   {
     path: "/",

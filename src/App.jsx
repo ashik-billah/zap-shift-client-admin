19070cab61deabab330b4a9089474df4fae9e4e8
ashik-billah <ashik.210101@s.pust.ac.bd>
2026-04-25 21:53:39 +0600

    add onclick buuton

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        {/* addL */}
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>

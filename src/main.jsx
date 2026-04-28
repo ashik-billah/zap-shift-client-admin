33b716ad74ae4cd97ede928cbcd131bb2e09e192
ashik-billah <ashik.210101@s.pust.ac.bd>
2026-04-28 21:27:03 +0600

    router dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from "react-router/dom";
 import './index.css'
+// add
 import App from './App.jsx'
 import { router } from './routes/router.jsx';
 import AuthProvider from './contexts/AuthContext/AuthProvider.jsx';

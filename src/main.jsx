42d65ee265d0ceeab98214061ccb0e7cb90426f6
ashik-billah <ashik.210101@s.pust.ac.bd>
2026-04-30 22:02:35 +0600

    add router

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 import { router } from './routes/router.jsx';
 import AuthProvider from './contexts/AuthContext/AuthProvider.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
+// add
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
